6176195d54237b2aab83e24ebc05c7b8e97238ba
malaypatoliya <malaypatoliya1619@gmail.com>
2023-03-26 16:39:59 +0530

    add product and update product created with bulkCreate

--- src/controllers/product/productController.js
+++ src/controllers/product/productController.js
@@ -5,80 +5,67 @@ const Category = db.categories;
 const Product = db.products;
 const ProductCategory = db.product_categories;
 const CryptoJS = require("crypto-js");
+const fs = require("fs");
+
+// make a function to save the file in public/products folder and get the file name
+const uploadImage = async (req, res) => {
+  try {
+    if (!req.file) {
+      return sendError(res, 400, false, "Image is required");
+    } else {
+      // read the file from req.file and save it in public/products folder and get the file name using readFileSync
+      // and save the file name in images column of product table
+
+      let filedata = fs.readFileSync(req.file.path);
+      return sendSuccess(res, 200, true, "Image uploaded successfully", { file: req.file, filedata: filedata });
+
+    }
+  } catch (error) {
+    return sendError(res, 500, false, "Something went wrong", error);
+  }
+};
 
 // add product
 const addProduct = async (req, res) => {
-  let {
-    title,
-    amount,
-    discount_type,
-    discount_amount,
-    avatar_image,
-    images,
-    short_description,
-    description,
-  } = req.body;
-
-  // generate slug from title and insert it into product table
-  const slug = title.toLowerCase()
-    .trim()
-    .replace(/[^\w\s-]/g, '')
-    .replace(/[\s_-]+/g, '-')
-    .replace(/^-+|-+$/g, '')
-
-
-  let category_id = CryptoJS.AES.decrypt(
-    req.header("category_id"),
-    process.env.SECRET_KEY
-  ).toString(CryptoJS.enc.Utf8);
-
   try {
-    // for loop for checking all fields are not empty
-    for (let key in req.body) {
-      if (req.body[key] === "")
-        return sendError(res, 400, false, `${key} is required`);
+    let { title, amount, discount_type, discount_amount, short_description, description, categoryArray } = req.body;
+    // check if all fields are filled
+    if (!title || !amount || !discount_type || !discount_amount || !short_description || !description) {
+      return sendError(res, 400, false, "All fields are required");
     }
-    // check if category_id is a number
-    if (isNaN(category_id))
-      return sendError(res, 400, false, "Category Id must be a number");
 
-    // check if category exists
-    const category = await Category.findOne({ where: { id: category_id } });
-    if (!category)
-      return sendError(res, 400, false, "Category does not exist");
+    // check categoryArray is not empty
+    if (!categoryArray || categoryArray.length === 0) {
+      return sendError(res, 400, false, "Category is required");
+    }
 
     // check if product already exists
     const product = await Product.findOne({ where: { title } });
-    if (product)
+    if (product) {
       return sendError(res, 400, false, "Product already exists");
+    }
 
-    //  crearte product and insert data into it and also insert category_id in product_category table
+    // create a product and insert data into it and also insert category_id in product_category table 
     const newProduct = await Product.create({
       title,
       amount,
       discount_type,
       discount_amount,
-      avatar_image,
-      images,
       short_description,
       description,
-      category_id,
-      slug
+      slug: title.toLowerCase().trim().replace(/[^\w\s-]/g, '').replace(/[\s_-]+/g, '-').replace(/^-+|-+$/g, '')
     });
     if (newProduct) {
-      // insert inserted product's id and  category_id in product_category table
-      const productCategory = await ProductCategory.create({
-        product_id: newProduct.id,
-        category_id,
+      // append produt_id in productCategoryArray
+      const productCategoryArray = [];
+      categoryArray.forEach((item) => {
+        productCategoryArray.push({ product_id: newProduct.id, category_id: item })
       });
-      if (productCategory)
-        return sendSuccess(
-          res,
-          201,
-          true,
-          "Product created successfully",
-          newProduct
-        );
+      // insert data into product_category table
+      const productCategory = await ProductCategory.bulkCreate(productCategoryArray);
+      if (productCategory) {
+        return sendSuccess(res, 200, true, "Product added successfully", newProduct);
+      }
     }
   } catch (error) {
     return sendError(res, 500, false, "Something went wrong", error);
@@ -132,7 +119,6 @@ const getProductByCategory = async (req, res) => {
         ).toString(CryptoJS.enc.Utf8),
       },
     });
-    console.log(category);
     if (category) {
       const products = await ProductCategory.findAll({
         where: {
@@ -174,77 +160,98 @@ const getProductByCategory = async (req, res) => {
 
 // update product
 const updateProduct = async (req, res) => {
-  let {
-    title,
-    amount,
-    discount_type,
-    discount_amount,
-    avatar_image,
-    images,
-    short_description,
-    description,
-  } = req.body;
-  let category_id = CryptoJS.AES.decrypt(
-    req.header("category_id"),
-    process.env.SECRET_KEY
-  ).toString(CryptoJS.enc.Utf8);
-  let product_id = CryptoJS.AES.decrypt(
-    req.header("product_id"),
-    process.env.SECRET_KEY
-  ).toString(CryptoJS.enc.Utf8);
   try {
-    // for loop for checking all fields are not empty
-    for (let key in req.body) {
-      if (req.body[key] === "")
-        return sendError(res, 400, false, `${key} is required`);
+    let {
+      title,
+      amount,
+      discount_type,
+      discount_amount,
+      short_description,
+      description,
+      categoryArray,
+    } = req.body;
+
+    let product_id = CryptoJS.AES.decrypt(
+      req.header("product_id"),
+      process.env.SECRET_KEY
+    ).toString(CryptoJS.enc.Utf8);
+
+    // check if all fields are empty or not
+    if (!title || !amount || !discount_type || !discount_amount || !short_description || !description) {
+      return sendError(res, 400, false, "All fields are required");
     }
-    // check if category_id is a number
-    if (isNaN(category_id))
-      return sendError(res, 400, false, "Category Id must be a number");
 
-    // check if product_id is a number
-    if (isNaN(product_id))
-      return sendError(res, 400, false, "Product Id must be a number");
+    // check if categoryArray is empty or not
+    if (!categoryArray || categoryArray.length === 0) {
+      return sendError(res, 400, false, "Category is required");
+    }
 
-    // check if category exists
-    const category = await Category.findOne({ where: { id: category_id } });
-    if (!category)
-      return sendError(res, 400, false, "Category does not exist");
-
-    // check if product already exists
+    // check product id is valid or not, if valid then update product details and also update product_category table 
     const product = await Product.findOne({ where: { id: product_id } });
-    if (!product)
-      return sendError(res, 400, false, "Product does not exist");
-
-    // update product
-    const updatedProduct = await Product.update(
-      {
-        title,
-        amount,
-        discount_type,
-        discount_amount,
-        avatar_image,
-        images,
-        short_description,
-        description,
-        category_id,
-      },
-      { where: { id: product_id } }
-    );
-    if (updatedProduct) {
-      // update category_id in product_category table
-      const productCategory = await ProductCategory.update(
-        { category_id },
-        { where: { product_id } }
+    if (product) {
+      const updatedProduct = await Product.update(
+        {
+          title,
+          amount,
+          discount_type,
+          discount_amount,
+          short_description,
+          description,
+          slug: title.toLowerCase().trim().replace(/[^\w\s-]/g, "").replace(/[\s_-]+/g, "-").replace(/^-+|-+$/g, ""),
+        },
+        { where: { id: product_id } }
       );
-      if (productCategory)
-        return sendSuccess(
-          res,
-          200,
-          true,
-          "Product updated successfully",
-          updatedProduct
+      if (updatedProduct) {
+        // if current category array is [1, 2, 4] and previous category array is [1, 2, 3] then it will delete 3 from product_category table and insert 4 into product_category table
+        let product_id = product.id;
+        let previousCategoryArray = [];
+        let currentCategoryArray = [];
+
+        // get all previous category id from product_category table
+        const previousCategory = await ProductCategory.findAll({
+          where: { product_id },
+        });
+        if (previousCategory.length > 0) {
+          previousCategory.forEach((item) => {
+            previousCategoryArray.push(item.category_id);
+          });
+        }
+
+        // get all current category id from categoryArray
+        categoryArray.forEach((item) => {
+          currentCategoryArray.push(item);
+        });
+
+        // get all category id which are not in previous category array
+        let insertCategoryArray = currentCategoryArray.filter(
+          (item) => !previousCategoryArray.includes(item)
         );
+
+        // get all category id which are not in current category array
+        let deleteCategoryArray = previousCategoryArray.filter(
+          (item) => !currentCategoryArray.includes(item)
+        );
+
+        // delete category id from product_category table using bulk delete
+        if (deleteCategoryArray.length > 0) {
+          await ProductCategory.destroy({
+            where: { product_id, category_id: deleteCategoryArray },
+            force: true,
+          });
+        }
+
+        // insert new category id into product_category table
+        if (insertCategoryArray.length > 0) {
+          let insertArray = [];
+          insertCategoryArray.forEach((item) => {
+            insertArray.push({ product_id, category_id: item, });
+          });
+          await ProductCategory.bulkCreate(insertArray);
+        }
+        return sendSuccess(res, 200, true, "Product updated successfully", { previousCategoryArray, currentCategoryArray, insertCategoryArray, deleteCategoryArray });
+      }
+    } else {
+      return sendError(res, 200, false, "Product not found");
     }
   } catch (error) {
     return sendError(res, 500, false, "Something went wrong", error);
@@ -256,4 +263,7 @@ module.exports = {
   getProductByCategory,
   updateProduct,
   addProduct,
+  uploadImage
 };
+
+
--- src/routes/product/productRoute.js
+++ src/routes/product/productRoute.js
@@ -4,6 +4,41 @@ const validateAdminToken = require('../../middlewares/validateAdminToken.js');
 
 const router = express.Router();
 
+const multer = require('multer');
+const path = require('path');
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, path.join(__dirname, '../../public/product'));
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + '-' + file.originalname);
+//     }
+// });
+
+// const upload = multer({ storage: storage });
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../../public/products'));
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+            return cb(new Error('Only image files are allowed!'), false);
+        }
+        cb(null, true);
+    }
+});
+
+router.post('/upload-avatar', upload.single('avatar_image'), productController.uploadImage);
 
 router.get('/get-product-by-id', productController.getProductById)
 router.get('/get-product-by-category-id', productController.getProductByCategory)
