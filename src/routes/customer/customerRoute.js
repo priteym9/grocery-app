6847b34766fdd595be0f687f1b4ec9daa6f441fe
Vikramsinh28 <vikramsinhparmar2812@gmail.com>
2023-04-05 12:12:42 +0530

    delete address api

--- src/routes/customer/customerRoute.js
+++ src/routes/customer/customerRoute.js
@@ -11,6 +11,7 @@ router.post('/add-customer-address', validateUserToken, customerController.addCu
 router.get('/get-customer-all-orders', validateUserToken, customerController.getCustomerAllOrders)
 router.put('/update-customer', validateUserToken, customerController.updateCustomer)
 router.put('/changePassword', validateUserToken, customerController.changePassword)
+router.delete('/delete-customer-address', validateUserToken, customerController.deleteAddress)
 
 module.exports = router;
 
