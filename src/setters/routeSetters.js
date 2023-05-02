e508aefb5e99d206c4944e4cf10e4d384f4a629e
malaypatoliya <malaypatoliya1619@gmail.com>
2023-05-02 16:44:56 +0530

    product image get api

--- src/setters/routeSetters.js
+++ src/setters/routeSetters.js
@@ -8,6 +8,8 @@ const paymentStatusRoute = require('../routes/paymentStatus/paymentStatusRoute.j
 // const { sendSuccess, sendError } = require('../utils/sendResponse.js');
 const APIResponseFormat = require('../utils/APIResponseFormat');
 const { _doEncrypt , _doDecrypt } = require('../utils/encryption.js');
+const path = require('path');
+const fs = require('fs');
 
 const router = express.Router();
 
@@ -41,6 +43,29 @@ router.get('/decryption', (req, res) => {
 router.get("/", (req, res) => {
     return APIResponseFormat._ResDataFound(res, "Welcome to the API");
 });
+
+// get image from public folder
+router.get('/get-image/:imgName', (req, res) => {
+    try{
+        const imgName = req.params.imgName
+        if(!imgName){
+            return APIResponseFormat._ResMissingRequiredField(res, 'image name is required');
+        }
+        let filePath = path.join(__dirname, '../', 'public', 'products', imgName);
+
+        if(fs.existsSync(filePath)){
+            // return image from path
+            return res.sendFile(filePath);
+        }else{
+            // return default image
+            return res.sendFile(path.join(__dirname, '../', 'public', 'products', 'default.png'));
+        }
+        // return APIResponseFormat._ResDataFound(res, 'Welcome to the API');
+    }catch(err){
+        return APIResponseFormat._ResServerError(res, err);
+    }
+});
+
 
 router.use('*', (req, res) => {
     return APIResponseFormat._ResRouteNotFound(res);
